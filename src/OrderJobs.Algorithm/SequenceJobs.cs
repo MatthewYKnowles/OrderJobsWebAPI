9c0682d8e2cb83a7afe4e2d1cdcd5b3ef7934f70
Matthew knowles <matthew.knowles@greatersum.com>
2016-11-30 12:06:27 -0500

    refactored where and aggregate functions into smaller methods

--- src/OrderJobs.Algorithm/SequenceJobs.cs
+++ src/OrderJobs.Algorithm/SequenceJobs.cs
@@ -36,12 +36,31 @@ namespace OrderJobs.Algorithm
 
         private string OrderJobs()
         {
-            _orderedJobs = _jobs.Where(job => job.Dependency == "").Aggregate(_orderedJobs, (acc, job) => acc + job.Name);
-            _orderedJobs = _jobs.Where(job => _orderedJobs.Contains(job.Dependency) && job.Dependency != "").ToList().Aggregate(_orderedJobs, (acc, job) => acc + job.Name);
-            _jobsRemaining = _jobs.Where(job => !_orderedJobs.Contains(job.Name));
-            _orderedJobs = _jobsRemaining.Where(job => _orderedJobs.Contains(job.Dependency) && job.Dependency != "").ToList().Aggregate(_orderedJobs, (acc, job) => acc + job.Name);
+            AddJobsWithNoDependencies();
+            CollectUnaddedJobs();
+            AddJobsWithDependenciesMet();
+            CollectUnaddedJobs();
+            AddJobsWithDependenciesMet();
 
             return _orderedJobs;
+        }
+
+        private void AddJobsWithDependenciesMet()
+        {
+            _orderedJobs =
+                _jobsRemaining.Where(job => _orderedJobs.Contains(job.Dependency))
+                    .ToList()
+                    .Aggregate(_orderedJobs, (acc, job) => acc + job.Name);
+        }
+
+        private void CollectUnaddedJobs()
+        {
+            _jobsRemaining = _jobs.Where(job => !_orderedJobs.Contains(job.Name));
+        }
+
+        private void AddJobsWithNoDependencies()
+        {
+            _orderedJobs = _jobs.Where(job => job.Dependency == "").Aggregate(_orderedJobs, (acc, job) => acc + job.Name);
         }
     }
 
