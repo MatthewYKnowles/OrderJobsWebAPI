adc2aadc6711c17fa585a04f70ed4fbc71a5ee84
Matthew knowles <matthew.knowles@greatersum.com>
2016-12-03 20:01:37 -0500

    Finished OrderedJobs Algorithm

---
+++ src/OrderJobsVerification.Algorithm/VerifyJobOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderJobsVerification.Algorithm
+{
+    public class VerifyJobOrder
+    {
+        private string v1;
+        private string v2;
+
+        public VerifyJobOrder()
+        {
+        }
+
+        public VerifyJobOrder(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public bool IsValid()
+        {
+            return true;
+        }
+    }
+}
---
+++ test/OrderJobsVerification.Test/OrderJobVerificationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OrderJobsVerification.Algorithm;
+
+namespace OrderJobsVerification.Test
+{
+    [TestFixture]
+    public class OrderJobVerificationTests
+    {
+        [Test]
+        public void EmptyJobListTest()
+        {
+            VerifyJobOrder verifyJobOrder = new VerifyJobOrder("", "");
+            Assert.That(verifyJobOrder.IsValid(), Is.EqualTo(false));
+        }
+    }
+}
